5e1edafed90fa420596eb71ec6ab2f18002fab3f
Hermann Krohn <krohnhermannacosta@gmail.com>
2022-01-28 17:35:49 -0500

    Adding a test to test out the PCA

--- gpio_test.py
+++ gpio_test.py
@@ -1,10 +1,14 @@
 from gpiozero import LED
 from time import sleep
+import PCA9685.Servo
 
-pin_17 = LED(17)
+PCA9685.Servo.test()
 
-while True:
-    pin_17.on()
-    sleep(1)
-    pin_17.off()
-    sleep(1)
+# pin_17 = LED(17)
+
+# while True:
+#     pin_17.on()
+#     sleep(1)
+#     pin_17.off()
+#     sleep(1)
+
