fc1cc17cc40b1d032d001492a82cf233aac9f1dd
Elikem Hermon <eliherm@gmail.com>
2022-02-05 18:50:40 -0500

    Adds init structure for main.py

--- main.py
+++ main.py
@@ -1,19 +1,57 @@
+import RPi.GPIO as GPIO # Import the GPIO library
+import time
+
+from motor_maintainer import MotorMaintainer
+from motor import Motor
+
+# =============
+# Pin constants
+# =============
+LEFT_AIN1_PIN = 13
+LEFT_AIN2_PIN = 11
+LEFT_PWM_PIN = 32
+RIGHT_AIN1_PIN = 13
+RIGHT_AIN2_PIN = 11
+RIGHT_PWM_PIN = 32
+STBY_PIN = 15
+
+# ================
+# Global variables
+# ================
+left_motor = None
+right_motor = None
+mtr_maintainer = None
+
 def setup():
-    pass
+    left_motor = Motor(LEFT_AIN1_PIN, LEFT_AIN2_PIN, LEFT_PWM_PIN)
+    right_motor = Motor(RIGHT_AIN1_PIN, RIGHT_AIN2_PIN, RIGHT_PWM_PIN)
+    mtr_maintainer = MotorMaintainer(left_motor, right_motor, STBY_PIN)
 
 
 def loop():
-    pass
+    while (1):
+        mtr_maintainer.nav_forward(50)
+        time.sleep(5)
+        mtr_maintainer.nav_backwards(50)
+        time.sleep(5)
+        mtr_maintainer.nav_left(50, 75)
+        time.sleep(5)
+        mtr_maintainer.nav_right(50, 75)
+        time.sleep(5)
+        mtr_maintainer.nav_brake()
+        time.sleep(5)
 
 
 def cleanup():
-    pass
+    mtr_maintainer.cleanup()
+    GPIO.cleanup()
 
 
 def main():
     setup()
     try:
         loop()
+        cleanup()
     except KeyboardInterrupt:
         cleanup()
 
